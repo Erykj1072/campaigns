42352ac3797fe003dc34d0d0a1300eeec2308af3
Erykj1072 <48418422+Erykj1072@users.noreply.github.com>
2020-08-25 13:02:16 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// CommonJS way of importing
+const express = require("express");
+
+// Express appilcation in app object
+const app = express();
+
+// Get route handler
+app.get("/", (req, res) => {
+  res.send({ bye: "buddy" });
+});
+
+// This will default the port to 5000 when in the development environment
+// When hosted the port will be dynamically provided by the hosting service
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
